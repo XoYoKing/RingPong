47cca23c00a0e3ea42e4aa55cc6fcddeff2d4ad1
Andrew Garber <andrew@andrewgarber.com>
2012-01-13 22:58:46 -0500

    Initial Commit

---
+++ RingPong/AGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AGAppDelegate.h
+//  RingPong
+//
+//  Created by splicer on 12-01-13.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AGAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ RingPong/AGAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AGAppDelegate.m
+//  RingPong
+//
+//  Created by splicer on 12-01-13.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AGAppDelegate.h"
+
+@implementation AGAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
